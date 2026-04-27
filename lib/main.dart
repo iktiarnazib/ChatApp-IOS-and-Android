acf6abe3c27a32a014cd38218b54f48ca29b6109
Iktiar Nazib <iktiarnazib@gmail.com>
2026-04-27 15:17:48 +0600

    Theme set to light mode, setted provider, will change later with switch

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,24 @@
 import 'package:chatapps/pages/login_page.dart';
+import 'package:chatapps/themes/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(ProviderScope(child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginPage());
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeMode = ref.watch(themeProvider);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: themeMode,
+      home: LoginPage(),
+    );
   }
 }
