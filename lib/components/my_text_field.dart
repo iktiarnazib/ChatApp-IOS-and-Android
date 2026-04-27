26afed6153582f9b4fa4b7dcffef3120e9bee65a
Iktiar Nazib <iktiarnazib@gmail.com>
2026-04-27 15:43:07 +0600

    Login ui improvements

---
+++ lib/components/my_text_field.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class MyTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+  final bool obscureText;
+  const MyTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: TextFormField(
+        //don't show password
+        obscureText: obscureText,
+        //controling the text
+        controller: controller,
+
+        //decorating
+        decoration: InputDecoration(
+          //normally the bordercolor white
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Theme.of(context).colorScheme.tertiary,
+            ),
+          ),
+          //when clicked border color grey[500] - primary
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+          //what text it shows when nothing is typed
+          hintText: hintText,
+          //hint text style
+          hintStyle: TextStyle(color: Theme.of(context).colorScheme.primary),
+          //what color will the textfield be
+          fillColor: Theme.of(context).colorScheme.secondary,
+          //will the textfield be filled with color
+          filled: true,
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,71 @@
+import 'package:chatapps/components/my_sign_button.dart';
+import 'package:chatapps/components/my_text_field.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passController = TextEditingController();
+
+  void onSignIn() async {}
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(height: 25),
+          //logo
+          SafeArea(
+            child: Center(
+              child: Icon(
+                Icons.message,
+                size: 60,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            ),
+          ),
+
+          //welcome back
+          Text(
+            'Welcome back, you\'ve been missed',
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+          ),
+
+          SizedBox(height: 25),
+
+          //email textfield
+          MyTextField(
+            controller: emailController,
+            hintText: 'Email',
+            obscureText: false,
+          ),
+
+          SizedBox(height: 10),
+
+          //password textfield
+          MyTextField(
+            controller: passController,
+            hintText: 'Password',
+            obscureText: true,
+          ),
+
+          //login button
+          MySignButton(buttonText: 'Sign In', onTap: onSignIn),
+
+          //register button
+        ],
+      ),
+    );
   }
 }
